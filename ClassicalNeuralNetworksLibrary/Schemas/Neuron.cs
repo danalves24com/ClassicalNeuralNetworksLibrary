f18a00ede09228e29e02d7a4a724c853f6bddaa5
Daniel Rosel <danalves24@outlook.com>
2021-04-27 17:01:22 +0200

    Add project files.

---
+++ ClassicalNeuralNetworksLibrary/Schemas/Neuron.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicalNeuralNetworksLibrary.Schemas
+{
+    class Neuron
+    {
+        public double[] weights { get; set; }
+        public double output { get; set; }
+        public double bias { get; set; }
+        public double pass(double[] inputs)
+        {
+            this.output = this.bias;
+            int index = 0;
+            foreach(double inp in inputs)
+            {
+                double produc = inp * weights[index];
+                this.output += produc;
+                index += 1;
+            }
+
+            return output;
+        }
+
+        public Neuron(double[] weights, double bias)
+        {
+            this.weights = weights;
+            this.bias = bias;
+        }
+
+
+        public void init(int dims)
+        {
+            this.weights = new double[dims];
+            this.bias = 0;
+        }
+
+        public Neuron()
+        {
+
+        }
+
+
+    }
+}
